32b3505c33184ba87e87a021bf3d46ff40bf9234
Goedel, Richard <Richard.Goedel.contractor@rle.de>
2025-08-08 15:45:40 +0200

    fix: remove dist not needed

--- dist/tsc/browser.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const main_1 = require("./main");
-window.meetergo = main_1.meetergo;
-//# sourceMappingURL=browser.js.map
